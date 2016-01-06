eb64fd89b5e3457ee0dbbf730079ef820c1b141c
Edgar Duran <edgarduran@Edgars-MacBook-Pro-6.local>
2016-01-06 14:29:51 -0700

    customers finished

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,7 +6,17 @@ class Customer < ActiveRecord::Base
   # this scope makes sure your db is ordered in sequential order
   # default_scope -> { order('id DESC') }
 
-  def favorite_merchant
-    # Customer.find_by(id: params[:id]).transactionswhere
+  def favorite_merchant(params)
+    invoices = Customer.find_by(id: params[:id]).transactions.where(result: "success").pluck(:invoice_id)
+    merchants = Invoice.find(invoices).map { |invoice| invoice.merchant_id }
+    get_mode(merchants)
   end
+
+  def get_mode(merchants)
+    count = Hash.new(0)
+    merchants.each {|x| count[x] += 1 }
+    id = count.sort_by{|k,v| v}.last[0]
+    name = Merchant.find_by(id: id)
+  end
+
 end
